fef4d38fbd6813ee247a15e05e9e7d4e2a837fcc
sysups <info@sysups.nl>
2018-07-18 16:43:24 +0200

    Removed debug echo command.

--- Remove-MSIPackage.ps1
+++ Remove-MSIPackage.ps1
@@ -1,8 +1,7 @@
 # First Searches for installed Java programms.
 $installedpackages = get-wmiobject Win32_Product -Property * | Where-Object Name -Like "*java*" | Select-Object LocalPackage
 
-# Thenremoves them using msiexec and the LocalPackagepath.
+# Then removes them using msiexec and the LocalPackagepath.
 foreach ($path in $installedpackages) {
-    Echo $path.LocalPackage
     msiexec.exe /x $path.LocalPackage /quiet
 }
